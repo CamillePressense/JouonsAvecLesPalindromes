ffa210be702ddae0592a01f1f4d118ac636ad111
Pressense Fasani <camillefasani@gmail.com>
2025-03-21 12:20:16 +0100

    Etape 4: Refacto fonctions isValidDate, Création fonctions isPalindrome et isDatePalindrome

--- script.js
+++ script.js
@@ -1,88 +1,3 @@
-/* Étape 1
-Créer une fonction isValidDate qui prend en paramètre une date en string et determine si elle est valide.
-Pour qu'une date soit valide, il faut qu'elle existe et qu'elle soit au format dd/mm/aaaa.
-Tout au long de l’exercice, on supposera des années supérieures à 999 et inférieures 9999 - 
-autrement dit, l’année sera systématiquement représentée sur 4 caractères.
-isValidDate("03/04/2001") // true isValidDate("03/14/2001") // false car 14 n'est pas un mois valide
-Vous aurez probablement besoin de créer une autre fonction maxDaysInMonth pour vous assurer 
-que le nombre de jours par mois est valide*/
-
-
-const DAYS = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19","20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"];
-const MONTHS = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
-const YEARA = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-const YEARBCD = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
-const MONTHSIN30 = ["04", "06", "09", "11"];
-const MONTHSIN31 = ["01", "03", "05", "07", "08", "10", "12"];
-const FEBRUARY = "02";
-
-function maxDaysInMonth(day, month){
-    if (MONTHSIN30.includes(month) && day === "31"){
-        return false
-    } else if (month === FEBRUARY && (day === "29" || day === "30" || day === "31")){
-        return false
-    } else {
-        return true
-    }
-}
-
-function isValidDate(strDate){
-    let result;
-    let dayOfStrDate = strDate[0] + strDate[1];
-    let montfOfStrDate = strDate[3] + strDate[4];
-
-    // Je vérifie le nombre de caractères 
-    if (strDate.length === 10){
-        //Je vérifie le format xx/xx/xxxx 
-        if (strDate[2] === "/" && strDate[5] === "/"){
-            //Je vérifie les jours (entre 01 et 31) et les mois ( entre 01 et 12)
-            if (DAYS.includes(dayOfStrDate) && MONTHS.includes(montfOfStrDate)){
-                //Je vérifie l'année (entre 1000 et 9999)
-                if (YEARA.includes(strDate[6]) && YEARBCD.includes(strDate[7]) && YEARBCD.includes(strDate[8]) && YEARBCD.includes(strDate[9]) )
-                result = true;   
-            } else {
-                result = false;
-            }
-        } else {
-            result = false;
-        }
-    } else {
-        result = false;
-    }
-    // Je vérifie que le nombre de jours correspond au nombre max de jours dans ce mois
-    if (result === true && maxDaysInMonth(dayOfStrDate, montfOfStrDate)){
-        result = true ;  
-    } else {
-        result = false;
-    }
-    console.log(result)
-    return result
-};
-
-/* Étape 2
-Créer une fonction isPalindrome qui prend une date en string en paramètre et retourne un booléen qui indique si la date est un palindrome. 
-Si la date est invalide, retourner false.
-Exemple de date palindrome: 11/02/2011. Les caractères / ne sont pas pris en compte.
-isPalindrome("11/02/2011") // true isPalindrome("03/04/2001") // false */
-
-function isPalindrome(strDate){
-    let result;
-    if (strDate[0] === strDate[9] &&
-        strDate[1] === strDate[8] &&
-        strDate[3] === strDate[7] &&
-        strDate[4] === strDate[6]) {
-            result = true
-        } else {
-            result = false
-        }
-    //console.log ( "la date est un palindrome : "+ result)
-    return result
-}
-
-isPalindrome("12/02/2021");
-isPalindrome("31/12/1986");
-isPalindrome("11/11/1111");
 
 /* Étape 3
 Créer une fonction getNextPalindromes qui donne les x prochaines dates palindromes à compter d’aujourd’hui. 
@@ -124,10 +39,75 @@ function getNextPalindromes(x){
     })
     console.log(nextPalindromes);
 }
+//getNextPalindromes(10)
 
-getNextPalindromes(10)
+/* Étape 4
+Refactorer la fonction isPalindrome pour faire en sorte qu’elle renvoie si, ou non, une chaine de caractère est un palindrome. 
+Créer ensuite une nouvelle fonction isDatePalindrome qui reprendra les spécificités du palindrome au format date (comme s’assurer que la date est valide) et qui appellera isPalindrome.
+ */
 
+//Je créée la fonction qui retourne true si la string entrée en paramètre est un palindrome.
+function isPalindrome(string){
+    let string2 = string.toLowerCase()
+    let result;
+    if (string2.split("").reverse().join("") === string2 ){
+    result = true;
+    } else {
+    result = false;
+    }
+    return result;
+}
 
+/* isPalindrome("ada");
+isPalindrome("martin");
+isPalindrome("radaR") */
 
+//Je créée la fonction qui vérifie si la date entrée en paramètre est une date valide. Elle retourne true si c'est le cas.
+function isValidDate(stringDate){
+    let result;
+    //Je récupère la partie jour-mois-année de la date en format dd/mm/yyyy
+    let day = stringDate[0] + stringDate[1];
+    let month = stringDate[3] + stringDate[4];
+    let year = stringDate[6] + stringDate[7] + stringDate[8] + stringDate[9]
+
+    //Je créé une vraie date à partir des variables day, month, year créées
+    let dateToCheck = new Date(); 
+    dateToCheck.setFullYear(year);
+    dateToCheck.setMonth(month - 1);
+    dateToCheck.setDate(day);
+
+    let dayOfRealDate = dateToCheck.getDate();
+    let monthOfRealDate = dateToCheck.getMonth() + 1;
+    let yearOfRealDate = dateToCheck.getFullYear();
+
+    // Je récupère les day, month, year de la date réelle créée et les compare à mes valeurs initiales
+    if ((day == dayOfRealDate) && (month == monthOfRealDate) && (year == yearOfRealDate)){
+        result = true;
+    } else {
+        result = false;
+    }
+    return result
+}
+    
+
+//Je créée la fonction qui vérifie si une date est un palindrome, en s'assurant d'abord qu'elle soit valide.
+function isDatePalindrome(stringDate){
+    if (isValidDate(stringDate) === true){
+        let date = stringDate.replaceAll("/", "");
+        if (isPalindrome(date) === true){
+            console.log(`La date ${stringDate} est un palindrome.`)
+        } else {
+            console.log(`La date ${stringDate} n'est pas un palindrome.`)
+        }
+    } else {
+        console.log(`La date ${stringDate} n'est pas une date valide.`)
+    }
+}
+
+isDatePalindrome("11/02/2011");
+isDatePalindrome("30/02/2005");
+isDatePalindrome("31/11/2019");
+isDatePalindrome("25/11/2018");
+isDatePalindrome("21/02/2012");
 
 
